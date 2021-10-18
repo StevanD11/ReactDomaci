243f2c79a9a2e6f1969a38720fea443d09f7255c
Stevan Draskovic <drasko1212@gmail.com>
2021-10-19 00:43:54 +0200

    Pocetna komponenta

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Nav';
+import Pocetna from './Pocetna';
 
 function App() {
-  return (<div className="App">
-    <Navbar />
-    <div className="content" >
-
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content" >
+        <Pocetna />
+      </div>
     </div>
-  </div>
   );
 }
 
---
+++ src/Pocetna.js
@@ -0,0 +1,9 @@
+const Pocetna = () => {
+    return (
+        <div className="pocetna">
+            <h2>Pocetna</h2>
+        </div>
+    );
+}
+
+export default Pocetna;
