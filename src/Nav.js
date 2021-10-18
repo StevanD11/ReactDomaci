a6f9c36c6011bd6dd09c8ae9ee01fe983af18b4a
Stevan Draskovic <drasko1212@gmail.com>
2021-10-19 00:38:13 +0200

    Navbar komponenta

---
+++ src/Nav.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>Kolacici</h1>
+            <div className="links">
+                <a href="/">Pocetna</a>
+                <a href="/kontakt">Kontakt</a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
